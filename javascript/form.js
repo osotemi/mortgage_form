8d5c61ac8c8925dbbbd53ce03e9b52c6e3fe684d
Oscar Otero <oscar.otero.millan@gmail.com>
2016-10-16 05:41:09 +0200

    Function result enabled, formula result enabled

--- javascript/form.js
+++ javascript/form.js
@@ -3,13 +3,28 @@ function Form( form_id ){
   //get form element
   this.form_id = form_id;
   this.form_elem = document.getElementById(this.form_id);
+  //Form checkBoxes elements
+  this.form_checkBoxes = [];
+  this.form_checkBoxesChecked = 0;
 
   var self=this; //Artifici binding
   this.getFormSelf = function(){return self;};
-  this.getActiveInput = function(){return document.activeElement;}
-  this.getActiveType = function(){return document.activeElement.getAttribute("pa-type");}
-  this.getActiveValue = function(){return document.activeElement.value;}
-  this.getSelectElement = function(){return document.getElementById("interest_rate_type");}
+  this.getActiveInput = function(){return document.activeElement;};
+  this.getActiveType = function(){return document.activeElement.getAttribute("pa-type");};
+  this.getActiveValue = function(){return document.activeElement.value;};
+  this.getSelectElement = function(){return document.getElementById("interest_rate_type");};
+  this.setCheckboxes = function(){
+    var inputs = this.form_elem.getElementsByTagName("input");
+    var checkBoxes = []; //will contain all checkboxes
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type == "checkbox") {
+        this.form_checkBoxes.push(inputs[i]);
+        if (inputs[i].checked) {
+          this.form_checkBoxesChecked += 1;
+        }
+      }
+    }
+  };
 
   Subject.call(this.prototype);
 
@@ -35,6 +50,18 @@ Form.prototype.changeRateType = function(){
   }
 }
 
+Form.prototype.enableResultFields = function( enable ){
+  var self_form = this.getFormSelf();
+  if( enable ){
+    self_form.form_elem.monthlyQuote.readonly = false;
+    self_form.form_elem.interestApplied.readonly = false;
+  }
+  else{
+    self_form.form_elem.monthlyQuote.readonly = true;
+    self_form.form_elem.interestApplied.readonly = true;
+  }
+}
+
 Form.prototype.getRateType = function(){
   var self_form = this.getFormSelf();
   var selected_interest = this.getSelectElement();
@@ -49,6 +76,33 @@ Form.prototype.getRateType = function(){
   return false;
 }
 
+Form.prototype.calculateResult = function( interest_type ){
+  var self_form = this.getFormSelf();
+  var interest = 0;
+  var cuota_mensual = 0;
+
+  self_form.setCheckboxes();
+  var active_products = self_form.form_checkBoxesChecked;
+  var income = parseInt(parseFloat(self_form.form_elem.ingresos_mensuales.value) * 100);
+  var capital = parseInt(parseFloat(self_form.form_elem.capital.value) * 100);
+  var period = parseInt(self_form.form_elem.period.value);
+  if(interest_type = "fixed"){
+    interest = parseInt(parseFloat(self_form.form_elem.fixed_interest.value) * 1000);
+    interest -= active_products * 50;
+  }
+  else{
+    var euribor = parseFloat(self_form.form_elem.euribor.value) * 1000;
+    var differential = parseFloat(self_form.form_elem.differential.value * 1000);
+    interest = parseInt(euribor) + parseInt(differential);
+    interest -= active_products * 50;
+  }
+  //cuota_mensual = parseInt((((capital/100) * (interest/1000) / 12) / (100 * ( 1- Math.round(Math.pow(1 + (( (interest / 1000) / 12 )/100 ) , (-1 * period * 12))) ))) * 100);
+  cuota_mensual = parseInt(( ((capital/100) * (interest/1000) / 12) / (100 * ( 1- Math.round(Math.pow(1 + (( (interest / 1000) / 12 )/100 ) , (-1 * period * 12))) ))) * 100);
+  self_form.form_elem.monthlyQuote.value = cuota_mensual/100;
+  self_form.form_elem.interestApplied.value = interest/1000;
+  self_form.form_elem.interestToPay.value = (((cuota_mensual * period * 12) - income) /100);
+};
+
 Form.prototype.notifyEventForm = function(){
     this.Notify(this);
 };
@@ -61,6 +115,6 @@ Form.prototype.start = function(){
 }
 
 Form.prototype.init = function(){
-  var self_form = this.getFormSelf();
-  document.getElementById('interest_rate_type').onchange = self_form.changeRateType;
+
+
 }
--- javascript/validator.js
+++ javascript/validator.js
@@ -4,6 +4,25 @@ function Validator( context ){
 
   this.context = context;
 
+  this.enabledResult = false;
+
+  this.validResultFields = {
+    valid_incoming : false,
+    valid_capital : false,
+    valid_interest : false,
+    valid_peiod : false,
+  };
+
+  this.validInterestRate = {
+    valid_euribor : false,
+    valid_differencial : false,
+    valid_fixed : false,
+  };
+
+  this.isValidRateType = function(){
+
+  };
+
   withObserver.call(Validator.prototype);
   //We enroll stick as a ball observer
 	this.context.form_.AddObserver(this);
@@ -14,6 +33,26 @@ function Validator( context ){
     this.current_value =form.getActiveValue();
 
     this.switchType();
+
+    //Si current es de RateType:
+
+    //Si current es de Result:
+    if ( this.enabledResult ){
+      form.calculateResult( "fixed" );
+      form.enableResultFields( true );
+    }
+    //Si current es de required:
+
+    /*
+    if( this.valid_ingresos &&  this.valid_capital && this.valid_period ){
+      if( this.valid_fix_interest ){
+        form.calculateResult( "fixed" );
+      }
+      else if (this.valid_){
+        form.calculateResult(  );
+      }
+    }
+    */
   }
 }
 
@@ -69,13 +108,98 @@ Validator.prototype.percentage = function(  ){
   }
 };
 
+Validator.prototype.number = function(  ){
+  var current_element = document.activeElement;
+  var num_pattern = /^[\d]{0,2}$/;//float personal percentage ([9.999 , -9.999] range) regular expresion
+
+  if (  this.current_value == "How many years?"){
+     this.current_element.value = "";
+     return false;
+  }
+  else if( this.current_value == "" ){
+    this.current_element.style.border = "1px solid red";
+    this.current_element.focus();
+    return false;
+  }
+  else{
+    if( ! num_pattern.test(this.current_value) ){
+       this.current_element.style.border = "1px solid red";
+       this.current_element.focus();
+       return false;
+    }
+    else{
+      this.current_element.style.border = "2px solid green";
+      return true;
+    }
+  }
+};
+
+Validator.prototype.check = function( is_valid ){
+  switch (this.current_element.id) {
+    case "ingresos_mensuales":
+      this.validResultFields.valid_incoming = is_valid;
+      break;
+    case "capital":
+      this.validResultFields.valid_capital = is_valid;
+      break;
+    case "euribor":
+      this.validInterestRate.valid_euribor = is_valid;
+      if ( this.validInterestRate.valid_euribor && this.validInterestRate.valid_differencial ){
+        this.validResultFields.valid_interest = true;
+      }
+      else{
+        this.validResultFields.valid_interest = false;
+      }
+      break;
+    case "differential":
+      this.validInterestRate.valid_differencial = is_valid;
+      if ( this.validInterestRate.valid_euribor && this.validInterestRate.valid_differencial ){
+        this.validResultFields.valid_interest = true;
+      }
+      else{
+        this.validResultFields.valid_interest = false;
+      }
+      break;
+    case "fixed_interest":
+      this.validInterestRate.valid_fixed = is_valid;
+      this.validResultFields.valid_interest = is_valid;
+      break;
+    case "period":
+      this.validResultFields.valid_peiod = is_valid;
+    default://text
+      this.valid = is_valid;
+  }
+
+};
+
+Validator.prototype.checkEnableResult = function(){
+
+  for ( var i = 0, len = this.validResultFields.length; i < len; i++ ) {
+      opt = this.validResultFields[i];
+      if ( opt === false ) {
+        this.enabledResult = false;
+      }
+  }
+  this.enabledResult = true;
+
+};
+
 Validator.prototype.switchType = function(){
   //validate.monthly_incoming(form_elem_input);
 
   switch (this.current_type) {
     case "money":
-      this.money(this.current_element);
+      this.check(this.money());
+
+      break;
+    case "dec_percentage":
+      this.check(this.percentage());
+      break;
+    case "number":
+      this.check(this.number());
       break;
     default://text
+      break;
   }
+  this.checkEnableResult();
 };
